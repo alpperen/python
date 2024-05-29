3978cc1e9312c6dc27432e5ee7d07f6cb9886834
Alperen <163494310+alpperen@users.noreply.github.com>
2024-05-29 18:58:20 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+def hesapla(ifade):
+    try:
+        return eval(ifade)
+    except:
+        return "Geçersiz ifade!"
+
+def hesap_makinesi():
+    print("Matematiksel İfadeler Hesap Makinesine Hoş Geldiniz!")
+    print("Çıkış yapmak için 'exit' yazın.")
+
+    while True:
+        ifade = input("Hesaplamak istediğiniz ifadeyi girin: ")
+
+        if ifade.lower() == "exit":
+            print("Hesap Makinesinden çıkılıyor...")
+            break
+
+        sonuc = hesapla(ifade)
+        print("Sonuç:", sonuc)
+
+if __name__ == "__main__":
+    hesap_makinesi()
